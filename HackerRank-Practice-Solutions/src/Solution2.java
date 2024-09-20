db146b0b779c062699073b1b68f97e7514ee4793
HarshadTile <tileharshad@gmail.com>
2024-09-20 22:37:31 +0530

    This is second solution

---
+++ HackerRank-Practice-Solutions/src/Solution2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        String myString  = scanner.next();
+        int myInt = scanner.nextInt();
+        scanner.close();
+        System.out.println(a); 
+        System.out.println(myString);
+        System.out.println(myInt);
+    }
+}

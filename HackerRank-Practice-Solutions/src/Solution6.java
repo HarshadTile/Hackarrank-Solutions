aefecc71d26d35ab82c3749873e899c3319abb74
HarshadTile <tileharshad@gmail.com>
2024-09-23 22:27:57 +0530

    7.Java Loops I Solution

---
+++ HackerRank-Practice-Solutions/src/Solution6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Solution6 {
+    
+    public static void main(String[]args){
+        
+        Scanner si = new Scanner(System.in);
+        
+        int num = si.nextInt();
+        
+        for(int i=1;i<=10;i++){
+            System.out.println(num+" x "+i+" = "+num*i);
+            
+        }
+         
+    }
+}
+
+
+

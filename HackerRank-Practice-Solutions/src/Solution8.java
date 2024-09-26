e4767ac4dd4209067e78a39de9c3b17d024f1145
HarshadTile <tileharshad@gmail.com>
2024-09-26 09:25:44 +0530

    9.Java Stdin and Stdout II Solution

--- HackerRank-Practice-Solutions/src/Solution8.java
+++ HackerRank-Practice-Solutions/src/Solution8.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
 
 public class Solution8 {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int linenumber = 1;
+        
+        while(scanner.hasNext())
+        {
+            String String_value = scanner.nextLine();
+            System.out.println(linenumber+" "+String_value);
+            linenumber++;
+        }
+        scanner.close();
+    }
 }
--- HackerRank-Practice-Solutions/src/Solution9.java
+++ HackerRank-Practice-Solutions/src/Solution9.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
 
 public class Solution9 {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int intvalue = scanner.nextInt();
+        double doublevalue = scanner.nextDouble();
+        scanner.nextLine(); // Consume the leftover newline character
+        String svalue = scanner.nextLine();
+        
+        System.out.println("String: " + svalue);
+        System.out.println("Double: " + doublevalue);
+        System.out.println("Int: " + intvalue);
+    }
 }
